06714d5583be6105aceab6cb927fa40b5c83e34a
Peptiks <77544374+Peptiks@users.noreply.github.com>
2024-04-15 18:00:19 +0100

    Changing the background window colour.

--- Main.c
+++ Main.c
@@ -10,27 +10,45 @@ int main( int argc, char* args[] ) {
     SDL_Init(SDL_INIT_EVERYTHING);
 
     SDL_Window *window = SDL_CreateWindow("ain't no way", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        if (NULL == window) {
+            printf("Could not create window: %s\n", SDL_GetError());
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 1;
+        }
 
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        if (renderer == NULL) {
+            printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 1;
+        }
 
-    if (NULL == window) {
-        printf("Could not create window: ");
-        return 1;
-    }
+    SDL_Surface* shapeSurface = SDL_CreateRGBSurface(0, 800, 600, 32, 0, 0, 0, 0);
+        if (shapeSurface == NULL) {
+            printf("SDL_Surface error: %s\n", SDL_GetError());
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return 1;
+        }
+
+    SDL_FreeSurface(shapeSurface);
+    SDL_SetRenderDrawColor(renderer, 40, 44, 52, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    // SDL_Delay(1000);
+    SDL_DestroyRenderer(renderer);
 
     SDL_Event windowEvent;
-    
-    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
 
     while (1) {
-
         if (SDL_PollEvent(&windowEvent)) {
 
             if (SDL_QUIT == windowEvent.type) {break;}
 
         }
     }
-    
 
     SDL_DestroyWindow(window);
     SDL_Quit();
