e6c127601d40dc888d7dd2ed8d5c31622b19d301
Peptiks <77544374+Peptiks@users.noreply.github.com>
2024-04-21 23:50:29 +0100

    I'm far too amateur to write C.

--- Main.c
+++ Main.c
@@ -3,58 +3,86 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
-const int SCREEN_WIDTH = 800, SCREEN_HEIGHT = 400;
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 400
 
-int main(int argc, char* args[]) {
-    printf("Hello, world");
-    SDL_Init(SDL_INIT_EVERYTHING);
-    
-    // FILE *file = fopen("src/data/Data.json", 'r');
+SDL_Window* InitialiseSDL() {
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("Error initialising SDL: ", SDL_GetError());
+        return NULL;
+    }
 
     SDL_Window *window = SDL_CreateWindow("ain't no way", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
-        if (NULL == window) {
-            printf("Could not create window: %s\n", SDL_GetError());
-            SDL_DestroyWindow(window);
-            SDL_Quit();
-            return 1;
-        }
+    if (NULL == window) {
+        printf("Error creating window: %s\n", SDL_GetError());
+        return NULL;
+    }
+
+    return window;
+}
+
+SDL_Renderer* CreateRenderer(SDL_Window *window) {
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-        if (renderer == NULL) {
-            printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-            SDL_DestroyWindow(window);
-            SDL_Quit();
-            return 1;
-        }
+    if (renderer == NULL) {
+        printf("Error Creating renderer: %s\n", SDL_GetError());
+        return NULL;
+    }
+    return renderer;
+}
 
-    SDL_Surface* shapeSurface = SDL_CreateRGBSurface(0, 800, 600, 32, 0, 0, 0, 0);
-        if (shapeSurface == NULL) {
-            printf("SDL_Surface error: %s\n", SDL_GetError());
-            SDL_DestroyWindow(window);
-            SDL_Quit();
-            return 1;
-        }
 
-    SDL_FreeSurface(shapeSurface);
+
+void Render(SDL_Renderer *renderer) {
     SDL_SetRenderDrawColor(renderer, 40, 44, 52, 255);
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
-    // SDL_Delay(1000);
-    SDL_DestroyRenderer(renderer);
+}
+
+void EventsHandler() {
 
     SDL_Event windowEvent;
+    while (SDL_PollEvent(&windowEvent)) {
 
-    while (1) {
-        if (SDL_PollEvent(&windowEvent)) {
-
-            if (SDL_QUIT == windowEvent.type) {break;}
-
+        if (windowEvent.type == SDL_QUIT) {
+            exit(0);
         }
-    }
 
+    }
+}
+
+void CleanUp(SDL_Renderer *renderer, SDL_Window *window) {
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    
+}
+
+void UpdateSpeed(int i) {
+    printf("%d", i);
+    i++;
+}
+
+int main(int argc, char* args[]) {
+    printf("Hello, world");
+
+
+    SDL_Window *window = InitialiseSDL();
+    if (window == NULL) {
+        return 1;
+    }
+
+    SDL_Renderer *renderer = CreateRenderer(window);
+    if (renderer == NULL) {
+        SDL_DestroyWindow(window);
+        return 1;
+    }
+    int i = 0;
+
+    while (1) {
+        EventsHandler();
+        Render(renderer);
+    }
 
     return 0;
 }
